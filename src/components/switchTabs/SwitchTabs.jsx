7caf75ba14eabb2f8d468477b5116a125e4c6db3
Anikesh-developer <anikesh.dsce98@gmail.com>
2023-05-12 11:25:07 +0530

    2nd update

--- src/components/switchTabs/SwitchTabs.jsx
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
 import './style.scss';
 
 
-const SwitchTabs = () => {
+const SwitchTabs = ({data , onTabChange}) => {
+
+  const [selectedTab , setSelectedTab] = useState(0);
+  const [left , setLeft] = useState(0);
+
+  const activeTab = (tab, index) => {
+    setLeft(index * 100)
+    setTimeout(() => {
+      setSelectedTab(index)
+    } , 300)
+    onTabChange(tab , index)
+  }
+
   return (
-    <div>
-      
+    <div className='switchingTabs'>
+      <div className="tabItems">
+        {data.map((tab , index) => (
+          <span key={index} className={`tabItem ${selectedTab === index ? "active" : ""}`} onClick={() => activeTab(tab, index)}>
+            {tab}
+          </span>
+        ))}
+        <span className='movingBg' style={{left}} />
+      </div>
     </div>
   )
 }
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 import ContentWrapper from '../../../components/contentwrapper/Contentwrapper';
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
-
+import useFetch from "../../../hooks/useFetch";
 
 const Trending = () => {
+
+  const [endpoint,setEndPoint] = useState("day");
+
+  const {data , loading} = useState() 
+
+  const onTabChange = (tab) => {};
+
   return (
     <div className='carouselSection'>
         <ContentWrapper>
             <span className="carouselTitle">Trending</span>
-            <SwitchTabs date={["Day" , "Week" ,"Months"]} />
+            <SwitchTabs date={["Day" , "Week" ,"Months"]} onTabChange={onTabChange} />
             {/* here we are passing prop as array because to make it dynamic example we can create Day wise , week wise or month wise etc*/}
         </ContentWrapper>
     </div>
